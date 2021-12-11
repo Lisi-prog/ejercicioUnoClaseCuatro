5fe5160a74f9ed7aaafe80fb8205b2211bf7b6c2
Lisi-prog <80710956+Lisi-prog@users.noreply.github.com>
2021-12-11 04:50:31 -0300

    el primer commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textTitle}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textTitle: {
+    marginTop: 150,
+    fontSize: 24,
+    color: 'orange',
   },
 });
